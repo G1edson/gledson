6f4cbce13a75fb56b5bda35d17852afb56b9c10e
Gledson Pereira de Miranda <g1edson@hotmail.com>
2024-12-22 22:00:40 +0000

    Arrumei a home em construção

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 export default function Home() {
   return (
-    <h1>Gledson Pereira de Miranda</h1>
-    <h3>Dominio para os meus estudos com programação web.</h3>
+    <span>
+      <h1>Gledson Pereira de Miranda</h1>
+      <h3>Dominio para os meus estudos com programação web.</h3>
+    </span>
   );
 }
