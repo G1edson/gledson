3acd113b958b72014aa4d632e6785a67011d08f8
Gledson Pereira de Miranda <g1edson@hotmail.com>
2025-01-10 00:27:52 +0000

    mudando a resposta do json

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,7 +4,11 @@ async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as soma;");
   console.log(result.rows);
 
-  response.status(200).json({ chave: "alunos do curso.dev é fora da média" });
+  const updatedAt = new Date().toISOString();
+
+  response.status(200).json({
+    updated_at: updatedAt,
+  });
 }
 
 export default status;
