89b8bfd40afb1a6e7f9732ca80163a04df4e301b
Gledson Pereira de Miranda <g1edson@hotmail.com>
2025-01-11 01:49:27 +0000

    adicionando   e

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,13 +1,31 @@
 import database from "infra/database.js";
+import { version } from "react";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as soma;");
-  console.log(result.rows);
-
   const updatedAt = new Date().toISOString();
+  const dataBaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = dataBaseVersionResult.rows[0].server_version;
+  const databaseMaxConnectionResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionValue =
+    databaseMaxConnectionResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
+  );
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
   });
 }
 
